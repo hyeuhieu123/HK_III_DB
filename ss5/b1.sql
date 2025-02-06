490c1128bff1a17cdc1f654e88591c1701d78dcb
hieu <nguyentrunghieuthptxp@gmail.com>
2025-02-06 11:41:13 +0700

    add

--- ss5/b1.sql
+++ ss5/b1.sql
@@ -1,43 +1,58 @@
+CREATE DATABASE ss5; 
 USE ss5;
 
-
 CREATE TABLE customers (
-    customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+    customer_id INT AUTO_INCREMENT PRIMARY KEY, 
+    customer_name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE,
     phone VARCHAR(15),
     address VARCHAR(255)
 );
 
-
 CREATE TABLE orders (
-    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT AUTO_INCREMENT PRIMARY KEY, 
     customer_id INT, 
     order_date DATE NOT NULL,
     total_amount DECIMAL(10, 2) NOT NULL,
-    status VARCHAR(50),
+    order_status VARCHAR(50),
     FOREIGN KEY (customer_id) REFERENCES customers(customer_id) 
 );
 
-INSERT INTO customers (name, email, phone, address)
-VALUES
-('A', 'nguyenvanan@example.com', '0901234567', '123 Le Loi, TP.HCM'),
-('B', 'tranthibich@example.com', '0912345678', '456 Nguyen Hue, TP.HCM'),
-('C', 'levancuong@example.com', '0923456789', '789 Dien Bien Phu, Ha Noi');
 
-INSERT INTO orders (customer_id, order_date, total_amount, status)
+INSERT INTO customers (customer_name, email, phone, address)
+VALUES
+('Nguyen Van An', 'nguyenvanan@example.com', '0901234567', '123 Le Loi, TP.HCM'),
+('Tran Thi Bich', 'tranthibich@example.com', '0912345678', '456 Nguyen Hue, TP.HCM'),
+('Le Van Cuong', 'levancuong@example.com', '0923456789', '789 Dien Bien Phu, Ha Noi');
+
+INSERT INTO orders (customer_id, order_date, total_amount, order_status)
 VALUES
 (1, '2025-01-10', 500000, 'Pending'),
 (1, '2025-01-12', 325000, 'Completed'),
 (NULL, '2025-01-13', 450000, 'Cancelled'),
 (3, '2025-01-14', 270000, 'Pending'),
 (2, '2025-01-16', 850000, NULL);
-
-
-select customers.customer_id,customers.name,customers.phone,orders.order_id,orders.status
- from customers left join orders
- on customers.customer_id=orders.customer_id;
-
-select customers.customer_id,customers.name,orders.order_id,orders.total_amount,orders.order_date
- from customers left join orders
- on customers.customer_id=orders.customer_id;
+select
+    od.order_id, 
+    od.order_date, 
+    od.total_amount, 
+    cs.customer_name, 
+    cs.email
+from orders od
+left join customers cs on  od.customer_id = cs.customer_id;
+select
+    cs.customer_id, 
+    cs.customer_name, 
+    cs.phone, 
+    od.order_id, 
+    od.order_status
+from customers cs
+left join orders od ON cs.customer_id = od.customer_id;
+select
+    cs.customer_id, 
+    cs.customer_name, 
+    od.order_id, 
+    od.total_amount, 
+    od.order_date
+FROM customers cs
+INNER JOIN orders od ON cs.customer_id = od.customer_id;
